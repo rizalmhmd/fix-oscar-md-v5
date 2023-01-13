a91082e88bd1fc790bc0cf56ed45eaafef5cc58f
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-13 19:38:22 +0700

    Create downloader-tiktok.js

---
+++ plugins/downloader-tiktok.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+let res = await fetch(`https://api.zeeoneofc.xyz/api/downloader/tiktok?url=${args[0]}&apikey=uNdFM7fR`)
+    if (!res.ok) throw await res.text()
+let json = await res.json()
+    await conn.sendButtonVid(m.chat, json.result.nowm, 'Nih Om', watermark, 'ᴀᴜᴅɪᴏ', `.ttaudio ${args}`, m)
+}
+handler.command = /^tiktok$/i
+handler.tags = ['downloader']
+handler.help = ['tiktok']
+handler.limit = false
+module.exports = handler
