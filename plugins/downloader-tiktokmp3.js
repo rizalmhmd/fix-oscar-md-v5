6f1d14bb72f4d6bad81fe080602188a9dff22189
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-13 19:42:53 +0700

    Create downloader-tiktokmp3.js

---
+++ plugins/downloader-tiktokmp3.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'url nya mana?'
+let res = await fetch(`https://api.zeeoneofc.xyz/api/downloader/tiktok?url=${args[0]}&apikey=uNdFM7fR`)
+ if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+    await conn.sendFile(m.chat, json.result.audio, 'ttaudio.mp3', m)
+}
+
+handler.help = ['ttaudio <url>']
+handler.tags = ['downloader']
+handler.command = /^ttaudio$/i
+
+
+module.exports = handler
