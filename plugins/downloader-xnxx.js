d9498b91452497215d148b1fd2727537b0c75fc0
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-13 18:29:40 +0700

    Update downloader-xnxx.js

--- plugins/downloader-xnxx.js
+++ plugins/downloader-xnxx.js
@@ -2,20 +2,11 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
     if (!text) throw `Harap masukan judulnya\n\n\nContoh : .xnxxsearch segs brutal`
     let res = await fetch(`https://malesin.xyz/xnxxsearch?q=${text}`)
-		let rest = await res.json()
-		let cap = `Hasil Pencarian Dari ${text}\n`
-	for (let v of rest.result) {
-	 cap += `• *Title :* ${v.title}
-• *Info :* ${v.info}
-• *Link :* ${v.link}
-`
-cap +=  '\n' + '••••••••••••••••••••••••' + '\n'
-    }
-  m.reply(cap)
-	} catch (e) {
-		console.error(e);
-		throw e;
-	}
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    let keqing = json.result.map((v, i) => `#${i + 1}. \n*Title:* ${v.title}\n*Info:* ${v.info}\n*Link:* ${v.link}\n==============\n`).join('\n') 
+    if (json.status) m.reply(keqing)
+    else throw json
 }
 handler.help = ['xnxxsearch <query>']
 handler.tags = ['asupan']
