1db898ae8654676843fe6ca057539c280a72efaf
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-13 21:03:16 +0700

    Update downloader-tiktokmp3.js

--- plugins/downloader-tiktokmp3.js
+++ plugins/downloader-tiktokmp3.js
@@ -4,7 +4,7 @@ if (!args[0]) throw 'url nya mana?'
 let res = await fetch(`https://zenzapis.xyz/downloader/tiktok?apikey=f9fccfcff1&url=${args[0]}`)
  if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
-    await conn.sendFile(m.chat, json.result.download.music, m)
+    await conn.sendFile(m.chat, json.result.music, m)
 }
 
 handler.help = ['ttaudio <url>']
